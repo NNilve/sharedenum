da3629aa61fd093b366f61b4cb41b486ba9b18bc
NNilve <nilve.eng@gmail.com>
2026-07-08 00:54:23 +0000

    Make `install-rokit-toolchain.ps1`run on repository root

--- scripts/install-rokit-toolchain.ps1
+++ scripts/install-rokit-toolchain.ps1
@@ -1,2 +1,13 @@
-Invoke-RestMethod https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.ps1 | Invoke-Expression
-rokit install
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$repoRoot = (Resolve-Path (Join-Path $PSScriptRoot "..")).Path
+Push-Location $repoRoot
+
+try {
+    Invoke-RestMethod https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.ps1 | Invoke-Expression
+    rokit install
+}
+finally {
+    Pop-Location
+}
